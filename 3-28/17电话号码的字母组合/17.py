c792d4cf5975185bb9e840c7a1a5ea005b35e081
Annihilation7 <763366463@qq.com>
2020-03-29 00:25:48 +0800

    [Feature] Add 3-28 Solutions

---
+++ 3-28/17电话号码的字母组合/17.py
@@ -0,0 +1,38 @@
+#  -*- coding: utf-8 -*-
+
+#  Editor      : Pycharm
+#  File        : 17
+#  Author      : ZhenyuMa
+#  Created     : 2020/3/28 11:44 下午
+#  Description : 回溯
+
+
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+
+        self.record = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        self.res = []
+        self.helper(digits, 0, '')
+        return self.res
+
+    def helper(self, digits, idx, cur_digit):
+        if idx == len(digits):
+            self.res.append(cur_digit)
+            return
+
+        for digit in self.record[digits[idx]]:
+            self.helper(digits, idx + 1, cur_digit + digit)
